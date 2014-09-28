987ea6d2671838baae22e190576d576b57416c77
Guliash <guliash92@gmail.com>
2014-09-28 22:47:48 +0400

    fix

--- kmpjs.js
+++ kmpjs.js
@@ -1,41 +1,44 @@
-var kmpjs = {
-
-    kmp: funtion(s, p) {
-        var n = s.length;
-        var m = p.length;
-        var prefix = this.calcPrefixFunction(p);
-        var res = [];
-        var q = -1;
-        for(var i = 0; i < n; i++) {
-            while(q >= 0 && p[q + 1] != s[i]) {
-                q = prefix[q];
+(function() {
+    var kmpjs = {
+        kmp: function(s, p) {
+            var n = s.length;
+            var m = p.length;
+            var prefix = this.calcPrefixFunction(p);
+            var res = [];
+            var q = -1;
+            for(var i = 0; i < n; i++) {
+                while(q >= 0 && p[q + 1] != s[i]) {
+                    q = prefix[q];
+                }
+                if(p[q + 1] == s[i]) {
+                    q++;
+                }
+                if(q == m - 1) {
+                    res.push(i - m + 1);
+                    q = prefix[q];
+                }
             }
-            if(p[q + 1] == s[i]) {
-                q++;
+            return res;
+        },
+        calcPrefixFunction: function(p) {
+            var n = p.length;
+            var prefix = [];
+            var q = -1;
+            prefix.push(q);
+            for(var i = 1; i < n; i++) {
+                while(q >= 0 && p[q + 1] != p[i]) {
+                    q = prefix[q];
+                }
+                if(p[q + 1] == p[i]) {
+                    q++;
+                }
+                prefix[i] = q;
             }
-            if(q == m - 1) {
-                res.push(i - m + 1);
-                q = prefix[q];
-            }
-        }
-        return res;
-    },
-
-    calcPrefixFunction(p) {
-        int n = p.length;
-        var prefix = [];
-        var q = -1;
-        prefix.push(q);
-        for(var i = 1; i < n; i++) {
-            while(q >= 0 && p[q + 1] != p[i]) {
-                q = prefix[q];
-            }
-            if(p[q + 1] == p[i]) {
-                q++;
-            }
-            prefix[i] = q;
-        }
-        return prefix;
-    },
-
-}
+            return prefix;
+        },
+    }
+    if (typeof define === 'function' && define.amd) define(function() { return kmpjs; });
+    else if (typeof module !== 'undefined') module.exports = kmpjs;
+    else if (typeof self !== 'undefined') self.kmpjs = kmpjs;
+    else window.kmpjs = kmpjs;
+})();
